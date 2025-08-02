13dc667eca5b562a749b70ca26daab8234d89c4f
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2025-08-02 13:19:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+//let score = "false"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN - not a number - type = number
+// true => 1 ; false => 0
+
+
+let isLoggedIn = "niti"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false
+// " " => false
+// "niti" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
