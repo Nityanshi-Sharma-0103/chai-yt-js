d6abe30ad8c36daf172e1f8cdda3e684876ef3d5
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2025-08-02 12:39:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "niti@google.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+
+// accountId = 2 // not allowed
+
+accountEmail = "ns@ns.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all code as newer version of js
+
+// alert("hello")// we are using node js not browser
+
+console.log(3 + 3);
+
+console.log("Hitesh") // code readability should be high
+
+let name = "Niti"
+let age = 23
+let isLoggedIn = true
+let state = null
+
+// number => 2 to power of 53
+// bigint
+// string =>
+// boolean = true/false 
+// null => standalone value
+// undefined 
+// symbol => unique 
+// object - imp
+
+console.log(typeof "Niti");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
