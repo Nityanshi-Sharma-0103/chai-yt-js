de60bf60968676936493a5b42c1e8a34abb2a74e
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2025-09-24 05:17:13 +0000

    variables and datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as a newer version
+
+// alert("Hello") // we are using node js , not browser
+// code redability should be high
+
+let name = "Hitesh" // string
+let age = 18 
+let isLoggedIn = true // boolean
+
+// number == 2 to power of 53
+// bigInt 
+// string
+// null == standalone value
+// undefined
+// symbol == unique
+// object
+console.log(typeof "Hitesh") 
+
+console.log(typeof null);
+console.log(typeof undefined);
+
