bcc248b6589e319630b3a22582e04b0f2e9b5f55
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-07 10:14:58 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive 
+// 7 types : string , number , boolean , null , undefined , symbol , bigint
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+const bigNumber = 345796423789014235844672n
+
+
+
+// Reference / non-primitive 
+
+
+const heroes = ["shaktiman" , "naagraj" , "doga"];
+
+let myObj = {
+    name: "niti",
+    age: 23,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+
+
+// arrays , objects , functions 
