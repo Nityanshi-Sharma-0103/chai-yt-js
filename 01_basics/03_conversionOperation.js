6f473dcd1aa556ee857359463797843d4de9dd7f
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-07 09:29:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
