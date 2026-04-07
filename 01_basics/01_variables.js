c22c6ff1f8651d047d0398a85124c5bffe909e55
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-07 06:53:50 +0000

    variables , datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "niti@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "ns@ns.com"
+accountPassword = "21212121"
+accountCity = "Indore"
+let accountState;
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use sttrict"; //treat this code as newer version
+
+// alert("hello") // we are using node not browser
+
+let name = "niti"
+let age = 18
+let isLoggedIn = false
+
+//number
+//string
+//bigint
+//boolean
+//null
+//undefined
+//symbol = unique
+
+//objects ==
+console.log(typeof "niti");
+console.log(typeof null);
+console.log(typeof undefined);
