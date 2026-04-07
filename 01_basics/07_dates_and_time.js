81f294dcb4dcf79ed19ddc75f295d4b59425427e
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-07 11:49:23 +0000

    date and time done

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,40 @@
+// DATES 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+// console.log(typeof myDate);
+
+// myCreatedDate = new Date(2026 , 0 , 23)
+// myCreatedDate = new Date(2026 , 0 , 23 , 5 , 3)
+myCreatedDate = new Date("04-07-2026")
+
+
+// console.log(myCreatedDate.toLocaleString());
+
+
+// ==============TIMESTAMP==================
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+
+newDate.toLocaleString('default' , {
+    weekday: "long"
+})
