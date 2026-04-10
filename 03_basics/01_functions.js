4007804bd3e2419052b5242975d931a5ccb77ccf
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-10 12:07:34 +0000

    functions intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,70 @@
+// const { useImperativeHandle } = require("react");
+
+
+// function sayMyName(){
+//     console.log("N");
+//     console.log("I");
+//     console.log("T");
+//     console.log("I");
+// }
+// sayMyName()
+
+// function addTwoNumbers(number1 , number2){
+//     console.log(number1 + number2);
+// }
+
+// function addTwoNumbers(number1 , number2){
+    
+//     // let result = number1 + number2
+//     // console.log("Hitesh");
+//     // return result
+//     return number1 + number2
+// }
+
+// const result = addTwoNumbers(7 , 9)
+// console.log("Result: ", result);
+
+
+
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// // console.log(loginUserMessage("Hitesh"))
+// console.log(loginUserMessage())
+
+
+
+// function calculateCartPrice(val1 , val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200 , 400 , 500 , 2000));
+
+// const user ={
+//     username : "Hitesh",
+//     price : 199,
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+// }
+// handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price: 399,
+// })
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 100]));
+
