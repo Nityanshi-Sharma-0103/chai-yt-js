d207215619187eba9a9f327fa471bbffe30b0f49
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-10 11:23:11 +0000

    objects and json api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,4 +51,28 @@ users[1].email
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor : "hitesh",
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = (prop.company) => {
+
+
+
+//  const navbar = ({company}) => {
+// }
+
+// navbar(company = "hitesh")
+
