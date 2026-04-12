61bc307eb72ed88ae861d6435cad31d8aeee451a
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-12 11:47:01 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,64 @@
 // var c = 300
-let a = 300
+// let a = 300
 
-if (true) {
-    let a = 10
-    const b = 20
-    // var c = 30
-    console.log("INNER: " , a);
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // var c = 30
+    // console.log("INNER: " , a);
     
-}
+// }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+
+// ===============NESTED SCOPE=============
+
+
+function one(){
+    const username = "Hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+
+// ============== INTERSTING ================
+
+
+console.log((addone(5)));
+
+
+function addone(num){
+    return num + 1
+}
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
