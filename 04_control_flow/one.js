75c26ffa7dc7d17723debc7a2c77bdef09609b38
Nityanshi Sharma <nityanshisharma0103@gmail.com>
2026-04-14 10:26:10 +0000

    Control flow in javascript

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -11,9 +11,38 @@
 // console.log("Execute");
 
 
-const score = 200
+// const score = 200
 
-if (score > 100) {
-    const power = "fly";
-    console.log(`User power: ${power}`)
+// if (score > 100) {
+//     let power = "fly";
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test")
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const UserLoggedIn = true 
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (UserLoggedIn && debitCard) {
+    console.log("Allow to buy courses");
+}
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User logged in");
+    
 }
